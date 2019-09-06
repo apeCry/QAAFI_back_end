1126917d023e2bfaee1694b1492582045010cc50
Ku X <ntstatus0xff@gmail.com>
2019-09-07 04:18:48 +1000

    add middleware to catch error

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes');
 const {connectToDB} =require('./utils/db');
+const errorHandler = require('./middleware/errorHandler')
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
+app.use(errorHandler);
 
 connectToDB()
   .then(()=>{
